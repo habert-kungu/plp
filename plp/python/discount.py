018daba0fcf5884136b5c6ec4e7328655b799eea
habert <habert231@gmail.com>
2024-05-13 14:00:35 +0300

    bug fix in python file

--- plp/python/discount.py
+++ plp/python/discount.py
@@ -1,10 +1,11 @@
-iddjdef cuculate_discount(price, discount_percent):
-   # price = int(input("Enter Original price item: "))
-   # discount_percent = int(input("Please Enter the Discount percent: "))
-    
-    final_price = discount_percent/100 * price 
+def cuculate_discount(price, discount_percent):
+    # price = int(input("Enter Original price item: "))
+    # discount_percent = int(input("Please Enter the Discount percent: "))
+
+    final_price = discount_percent / 100 * price
     final_price = round(final_price, 2)
-    
+
     print(f"$ {final_price}")
+
 
 cuculate_discount(50, 20)
